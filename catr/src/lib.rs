de3ea8e66ce4872aab99b1b1cf4719277d02e360
mixnuts07 <french0719@gmail.com>
2024-05-18 14:26:56 +0900

    wip catr

---
+++ catr/src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use clap::{App, Arg};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn run(config: Config) -> MyResult<()> {
+    for file_name in config.files {
+        println!("{}", file_name);
+    }
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("mixnuts")
+        .about("Rust Cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")   
+                .help("Input Files(s)")
+                .multiple(true)
+                .default_value("-")
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank_lines")
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblack")
+                .help("Number non-blank lines")
+                .takes_value(false)
+        )
+        .get_matches();
+
+    Ok(Config{
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblack"),
+    })
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool
+}
